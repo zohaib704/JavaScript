e1fb560aa5bbec2c73fd42ce3cc413cbcd037865
zohaib704 <85876226+zohaib704@users.noreply.github.com>
2024-02-01 10:40:49 +0000

    Update promises

--- 05_Iterations/reduce.js
+++ 05_Iterations/reduce.js
@@ -1,11 +1,11 @@
-const myNums = [1, 2, 3]
+// const myNums = [1, 2, 3]
 // const myTotal = myNums.reduce(function(acc, currval) {
 //     console.log(`acc is ${acc} currentvalue is ${currval}`);
 //     return acc + currval
 // }, 0)
 
-const myTotal = myNums.reduce((acc, curval)=> (acc + curval), 0)
-console.log(myTotal);
+// const myTotal = myNums.reduce((acc, curval)=> (acc + curval), 0)
+// console.log(myTotal);
 
 const shoppingCart = [
     {
